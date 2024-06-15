9d87222daeef8e4a019a9ce3e11f28d707d0b422
hassanelq <hassanelqadi2@gmail.com>
2024-06-15 06:05:37 +0100

    refactor: Update LottieAnimation zIndex value, add background color to body, and optimize Hero component

---
+++ app/components/about.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <section>
+      <h2 className="text-center text-[2.7rem] font-semibold text-transparent !bg-clip-text [background:linear-gradient(90deg,_#d4d4d4,_#797979)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+        About Me
+      </h2>
+    </section>
+  );
+};
+
+export default About;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
 import Hero from "./components/Hero";
 import Contact from "./components/contact";
 import Projects from "./components/projects";
+import About from "./components/about";
+import TracingBeam from "../utils/tracing-beam.js";
 
 export default function Home() {
   return (
-    <div className="">
-      <Hero />
-      <Projects />
-      <Contact />
+    <div>
+      <TracingBeam>
+        <Hero />
+        <Projects />
+        <About />
+        <Contact />
+      </TracingBeam>
     </div>
   );
 }
