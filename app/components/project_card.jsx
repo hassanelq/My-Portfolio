e5a7561a5ebbd307f8e886d396732103a0635385
hassanelq <hassanelqadi2@gmail.com>
2024-06-28 23:21:50 +0100

    refactor: Add @tailwindcss/line-clamp plugin for truncating project descriptions

--- app/components/project_card.jsx
+++ app/components/project_card.jsx
@@ -1,27 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 const ProjectCard = ({ project }) => {
   return (
-    <div className="flex w-[350px] lg:w-[400px] p-[6px] flex-col items-start gap-[6px] rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
+    <Link
+      href={project.githubRepo}
+      target="_blank"
+      className="flex w-[350px] lg:w-[400px] p-[6px] flex-col items-start gap-[6px] rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter"
+    >
       <Image src={project.image} className="rounded-[25px]"></Image>
-      <div className="pt-1 pb-3 px-5">
+      <div className="pt-1 pb-3 px-5 w-[100%]">
         <h2 className="text-[18px] lg:text-[20px] font-medium">
           {project.title}
         </h2>
-        <p className="text-[14px] lg:text-[16px] pt-1 pb-2 opacity-65 font-light">
+        <p className="text-[14px] lg:text-[16px] pt-1 mb-2 opacity-65 font-light line-clamp-3">
           {project.description}
         </p>
-        <div className="flex gap-1">
+        <div className="flex flex-wrap gap-1">
           {project.tags.map((tag, index) => (
             <span
               key={index}
-              className="bg-[#F5F5F5CC] rounded-[10px] px-[10px] py-[5px] text-[black] text-[12px] lg:text-[14px] font-medium"
+              className="bg-[#f5f5f594] rounded-[10px] px-[10px] py-[5px] text-[black] text-[12px] lg:text-[14px] font-medium"
             >
               {tag}
             </span>
           ))}
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- database/projects.js
+++ database/projects.js
@@ -1,4 +1,5 @@
 import test from "../public/images/projects/projet1.png";
+import clusteringProj from "../public/images/projects/clusteringProj.png";
 const projects = [
   {
     type: "web development",
@@ -6,12 +7,14 @@ const projects = [
       "A personal portfolio website built with Next.js and Tailwind CSS.",
     projects: [
       {
-        title: "Website Portfolio Template test",
+        title: "Clustering Visualization App - Academic project",
         description:
-          "A personal portfolio website built with Next.js and Tailwind CSS.",
-        image: test,
-        tags: ["Next.js", "Tailwind CSS"],
-        githubRepo: "/",
+          "I developed an interactive Clustering Visualization App using Streamlit to simplify clustering analysis for data scientists and enthusiasts. This app supports K-Means and Hierarchical clustering, integrates the Elbow Method for optimal cluster determination, and offers multiple data input methods (file upload, manual input, and random datasets).",
+        image: clusteringProj,
+        tags: ["Streamlit", "Python", "Pandas", "NumPy"],
+        githubRepo:
+          "https://github.com/hassanelq/Kmeans-and-herarchical-Clustering/",
+        Demo: "https://kmeans-and-herarchicalc.streamlit.app/",
       },
       {
         title: "Website Portfolio Template test",
