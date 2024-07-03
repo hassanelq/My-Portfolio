61e64728185734baf9a2395ff3f2e76ddbada896
hassanelq <hassanelqadi2@gmail.com>
2024-07-03 22:31:59 +0100

    refactor: Improve CertifCard component responsiveness and update CSS styling

--- app/components/CertifCard.jsx
+++ app/components/CertifCard.jsx
@@ -4,15 +4,17 @@ import credlyLogo from "../../public/images/Logos/credly.png";
 
 const CertifCard = ({ data }) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-4 justify-center items-center px-6 py-3 rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
+    <div className="flex flex-col sm:flex-row gap-0 sm:gap-4 justify-center items-center px-6 py-3 rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
       <Image
         src={data.IssuerLogo}
         width={80}
         height={80}
         className="p-2"
       ></Image>
-      <div className="w-[100%]">
-        <h2 className="text-[18px] lg:text-[18px] font-medium">{data.title}</h2>
+      <div className="w-[100%] text-center sm:text-left">
+        <h2 className="text-[18px] lg:text-[18px] font-medium  ">
+          {data.title}
+        </h2>
 
         <p className="text-[14px] lg:text-[16px] opacity-65 font-light ">
           {data.Date}
