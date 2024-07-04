a596d6131f8393d347904bae561a829b0c5af692
hassanelq <hassanelqadi2@gmail.com>
2024-07-04 02:12:40 +0100

    refactor: Update project card component to include GitHub and demo links

--- app/components/project_card.jsx
+++ app/components/project_card.jsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import githubLogo from "../../public/images/Logos/github.png";
+import DemoIcon from "../../public/images/Logos/demo.svg";
+
 const ProjectCard = ({ project }) => {
   return (
-    <Link
-      href={project.githubRepo}
-      target="_blank"
-      className="flex w-[350px] lg:w-[400px] p-[6px] flex-col items-start gap-[6px] rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter"
-    >
+    <div className="flex h-fit w-[350px] lg:w-[400px] p-[6px] flex-col items-start gap-[6px] rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
       <Image src={project.image} className="rounded-[25px]"></Image>
       <div className="pt-1 pb-3 px-5 w-[100%]">
         <h2 className="text-[18px] lg:text-[20px] font-medium">
           {project.title}
         </h2>
-        <p className="text-[14px] lg:text-[16px] pt-1 mb-2 opacity-65 font-light line-clamp-3">
+        <p className="text-[14px] lg:text-[16px] pt-1 mb-2 opacity-65 font-light ">
+          {/* line-clamp-3 */}
           {project.description}
         </p>
         <div className="flex flex-wrap gap-1">
@@ -25,8 +25,34 @@ const ProjectCard = ({ project }) => {
             </span>
           ))}
         </div>
+        <div className="flex gap-2 pt-3">
+          {project.githubRepo && (
+            <Link
+              href={project.githubRepo}
+              target="_blank"
+              className="flex gap-3 items-center 	mt-2 py-2 px-4 w-fit rounded-[25px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.10)_0%,_rgba(255,_255,_255,_0.05)_100%)]"
+            >
+              <Image src={githubLogo} height={22}></Image>
+              <p className="text-[14px] lg:text-[15px] opacity-65 font-light">
+                Github repo
+              </p>
+            </Link>
+          )}
+          {project.demo && (
+            <Link
+              href={project.demo}
+              target="_blank"
+              className="flex gap-3 items-center 	mt-2 py-2 px-4 w-fit rounded-[25px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.10)_0%,_rgba(255,_255,_255,_0.05)_100%)]"
+            >
+              <Image src={DemoIcon} height={25}></Image>
+              <p className="text-[14px] lg:text-[15px] opacity-65 font-light">
+                Demo
+              </p>
+            </Link>
+          )}
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
--- database/projects.js
+++ database/projects.js
@@ -1,59 +1,68 @@
-import test from "../public/images/projects/projet1.png";
 import clusteringProj from "../public/images/projects/clusteringProj.png";
+import ordinalsBot from "../public/images/projects/ordinalsBot.jpg";
+import ambcheck from "../public/images/projects/ambcheck.png";
+import ensaa from "../public/images/projects/ensaa.png";
+
 const projects = [
   {
     type: "web development",
     description:
-      "A personal portfolio website built with Next.js and Tailwind CSS.",
+      "A collection of web development projects showcasing various technologies and frameworks.",
     projects: [
       {
-        title: "Clustering Visualization App - Academic project",
+        title: "Ensaa.ma - Website for ADE, Clubs, and the ENSA Community",
         description:
-          "I developed an interactive Clustering Visualization App using Streamlit to simplify clustering analysis for data scientists and enthusiasts. This app supports K-Means and Hierarchical clustering, integrates the Elbow Method for optimal cluster determination, and offers multiple data input methods (file upload, manual input, and random datasets).",
-        image: clusteringProj,
-        tags: ["Streamlit", "Python", "Pandas", "NumPy"],
-        githubRepo:
-          "https://github.com/hassanelq/Kmeans-and-herarchical-Clustering/",
-        Demo: "https://kmeans-and-herarchicalc.streamlit.app/",
+          "A dynamic platform for the ENSA community, providing news updates, information about ADE and clubs, and press releases. Designed with Next.js for a responsive and user-friendly experience.",
+        image: ensaa,
+        tags: ["Next.js", "Git", "GitHub", "Figma", "SCSS"],
+        githubRepo: "https://github.com/XcodeXcodeX/Ensaa.ma",
+        demo: "https://ensaa.ma/",
       },
       {
-        title: "Website Portfolio Template test",
+        title: "AMBcheck - NFT Discord Bot and Web App",
         description:
-          "A personal portfolio website built with Next.js and Tailwind CSS.",
-        image: test,
-        tags: ["Next.js", "Tailwind CSS"],
-        githubRepo: "/",
-      },
-      {
-        title: "Website Portfolio Template test",
-        description:
-          "A personal portfolio website built with Next.js and Tailwind CSS.",
-        image: test,
-        tags: ["Next.js", "Tailwind CSS"],
-        githubRepo: "/",
-      },
-      {
-        title: "Website Portfolio Template test",
-        description:
-          "A personal portfolio website built with Next.js and Tailwind CSS.",
-        image: test,
-        tags: ["Next.js", "Tailwind CSS"],
-        githubRepo: "/",
+          "A comprehensive solution for user management in Ordinals communities, combining a Discord bot with a web app. Features include NFT ownership verification, wallet linking, and real-time updates.",
+        image: ambcheck,
+        tags: [
+          "Node.js",
+          "Next.js",
+          "MongoDB",
+          "Discord.js",
+          "Figma",
+          "GitHub",
+          "Git",
+        ],
+        demo: "https://www.ambcheck.com/",
       },
     ],
   },
   {
     type: "data science",
     description:
-      "A personal portfolio website built with Next.js and Tailwind CSS.",
+      "Projects demonstrating the application of data science techniques and tools.",
     projects: [
       {
-        title: "Website Portfolio Template",
+        title: "Clustering Visualization App - Academic Project",
         description:
-          "A personal portfolio website built with Next.js and Tailwind CSS.",
-        image: test,
-        tags: ["Next.js", "Tailwind CSS"],
-        githubRepo: "/",
+          "An interactive app developed with Streamlit to simplify clustering analysis. Supports K-Means and Hierarchical clustering, integrates the Elbow Method, and offers multiple data input methods.",
+        image: clusteringProj,
+        tags: ["Streamlit", "Pandas", "NumPy", "Matplotlib"],
+        githubRepo:
+          "https://github.com/hassanelq/Kmeans-and-herarchical-Clustering/",
+        demo: "https://kmeans-and-herarchicalc.streamlit.app/",
+      },
+    ],
+  },
+  {
+    type: "automation",
+    description: "Projects focused on automation and real-time data tracking.",
+    projects: [
+      {
+        title: "Ordinals Sales Bot - Real-Time Sales Tracking and Alerts",
+        description:
+          "A solution for tracking and alerting sales activity within the Ordinals NFT ecosystem. Integrates with Discord and Twitter to provide real-time updates, customizable alerts, and comprehensive sales data.",
+        image: ordinalsBot,
+        tags: ["Node.js", "Discord.js", "Twitter API", "Linux Server"],
       },
     ],
   },
