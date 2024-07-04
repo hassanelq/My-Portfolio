8b3e303e481c434652cc442f10cf57a5bbd9a067
hassanelq <hassanelqadi2@gmail.com>
2024-07-04 03:01:26 +0100

    refactor: Improve EduExpCard component responsiveness and update CSS styling

--- app/components/EduExpCard.jsx
+++ app/components/EduExpCard.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const EduExpCard = ({ data }) => {
   return (
-    <div className="flex gap-4 justify-center items-center px-6 py-3 rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
+    <div className="flex gap-2 sm:gap-4 justify-center items-center px-4 sm:px-6 py-3 rounded-[32px] border-[0px] bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(255,_255,_255,_0.20)_0%,_rgba(255,_255,_255,_0.02)_100%)] [box-shadow:0px] backdrop-filter">
       <Image
         src={data.IssuerLogo}
         width={80}
         height={80}
-        className="p-2"
+        className="p-0 sm:p-2 scale-75 sm:scale-100"
       ></Image>
       <div className="w-[100%]">
         <h2 className="text-[18px] lg:text-[18px] font-medium">{data.title}</h2>
