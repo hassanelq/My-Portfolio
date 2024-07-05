a11b2815346788a6fdad7dcd20ae3e5601402f9c
hassanelq <hassanelqadi2@gmail.com>
2024-07-05 15:25:04 +0100

    refactor: Improve responsiveness of CertifCard and EduExpCard components, update image paths, and optimize social media thumbnails

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,9 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hassan EL QADI",
-  description: "Finance & Decision-Making Engineering Student",
+  title: "Hassan EL QADI - Finance & Decision-Making Engineering Student",
+  description:
+    "3rd-year Finance & Decision-Making Engineering student at ENSA Agadir. Passionate about data science, blockchain, and innovative tech.",
 };
 
 export default function RootLayout({ children }) {
@@ -20,36 +21,56 @@ export default function RootLayout({ children }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* HTML Meta Tags */}
+        <title>
+          Hassan EL QADI - Finance & Decision-Making Engineering Student
+        </title>
+        <meta
+          name="description"
+          content="3rd-year Finance & Decision-Making Engineering student at ENSA Agadir. Passionate about data science, blockchain, and innovative tech."
+        />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:url" content="https://elqadi.me/" />
+        <meta property="og:type" content="website" />
         <meta
           property="og:title"
           content="Hassan EL QADI - Finance & Decision-Making Engineering Student"
         />
         <meta
           property="og:description"
-          content="Frontend Developer and Data Scientist with a focus on finance and decision-making."
+          content="3rd-year Finance & Decision-Making Engineering student at ENSA Agadir. Passionate about data science, blockchain, and innovative tech."
         />
         <meta
           property="og:image"
-          content="https://elqadi.me/images/social-thumbnail.png"
+          content="https://www.elqadi.me/images/social-thumbnail.png"
         />
-        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:url" content="https://elqadi.me" />
-        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="elqadi.me" />
+        <meta property="twitter:url" content="https://elqadi.me/" />
         <meta
           name="twitter:title"
           content="Hassan EL QADI - Finance & Decision-Making Engineering Student"
         />
         <meta
           name="twitter:description"
-          content="Frontend Developer and Data Scientist with a focus on finance and decision-making."
+          content="3rd-year Finance & Decision-Making Engineering student at ENSA Agadir. Passionate about data science, blockchain, and innovative tech."
         />
         <meta
           name="twitter:image"
-          content="https://elqadi.me/images/social-thumbnail.png"
+          content="https://www.elqadi.me/images/social-thumbnail.png"
         />
+        <meta
+          name="twitter:image:alt"
+          content="A thumbnail of Hassan EL QADI's portfolio"
+        />
+
+        {/* Robots Meta Tags */}
+        <meta name="robots" content="index, follow" />
       </Head>
       <body className={`${inter.className} overflow-x-hidden`}>
         <TracingBeam>
