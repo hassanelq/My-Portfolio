2203565a18b890356bd19fc1c22fe138b85d59d6
hassanelq <hassanelqadi2@gmail.com>
2024-07-05 17:04:01 +0100

    refactor: Improve responsiveness of Projects component and update CSS styling

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "./project_card";
 
 const Projects = () => {
   return (
-    <section id="projects" className="px-[3.6vw] sm:px-[8vw]">
+    <section id="projects" className="px-[3.6vw] w-screen sm:px-[8vw]">
       <h2 className="text-center pb-5 text-[2.3rem] sm:text-[2.5rem] md:text-[2.7rem] font-semibold text-transparent !bg-clip-text [background:linear-gradient(90deg,_#d4d4d4,_#797979)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
         Featured Projects
       </h2>
