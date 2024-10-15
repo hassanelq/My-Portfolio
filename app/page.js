853a5fb3d55a46706e8f9874965fbda31916d1f1
hassanelq <hassanelqadi2@gmail.com>
2024-10-15 17:49:53 +0100

    Remove commented out ChatBot component

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import Hero from "./components/Hero";
 import Contact from "./components/contact";
 import Projects from "./components/projects";
 import About from "./components/about";
-import ChatBot from "./components/chatbot";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
       <Projects />
       <About />
       <Contact />
-      {/* <ChatBot /> */}
     </div>
   );
 }
